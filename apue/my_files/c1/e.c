20f64e87d9bcce46a3ec00012f8d60b55621f2f8
wanghui.p15v <hui.wang@bitech-automotive.com>
2024-07-15 00:05:28 +0800

    add my apue files

---
+++ apue/my_files/c1/e.c
@@ -0,0 +1,25 @@
+#include <apue.h>
+
+#define BUFFSIZE    4096
+
+int main(void)
+{
+    int n;
+    char buf[BUFFSIZE];
+
+    while (n = read(STDIN_FILENO, buf, BUFFSIZE) >0) 
+    {
+        if(write(STDOUT_FILENO, buf, n) != n) 
+        {
+            ;
+        }
+    }
+
+    if (n < 0)
+    {
+        ;
+    }
+
+
+    exit(0);
+}
